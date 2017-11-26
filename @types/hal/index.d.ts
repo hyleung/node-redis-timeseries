624a464340302e08ca9d20c1b68d58982fca3732
Ho Yan Leung <hy.leung@gmail.com>
2017-11-25 22:11:16 -0800

    Add def for toJSON and toXML on Link

--- @types/hal/index.d.ts
+++ @types/hal/index.d.ts
@@ -2,7 +2,10 @@ declare module 'hal' {
     export interface Href {
         href:string
     }
-    export interface Link {}
+    export interface Link {
+        toJSON():object
+        toXML():string
+    }
     export interface Resource {
         link(link:Link): Resource;
         embed(rel:string, resource:Resource, pluralize:boolean):Resource
