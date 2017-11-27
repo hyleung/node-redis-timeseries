4026410424aa7ced408a027364c09504054fa16c
Ho Yan Leung <hy.leung@gmail.com>
2017-11-26 21:25:11 -0800

    Add hit tracker

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 //index.ts
 import * as express from 'express';
 import * as hal from 'hal';
+import * as redis from 'redis';
+import { RedisClient } from 'redis';
 
+const Timeseries = require('redis-timeseries');
+const client:RedisClient = redis.createClient();
+const rts = new Timeseries(client, "stats", {
+    '1hour' : {ttl: 24*60*60, duration:60*60}
+});
 let app = express();
 let indent = 2;
 
@@ -21,7 +28,13 @@ app.get('/events/form', function(req, res) {
 });
 
 app.post('/events', function(req, res) {
+    rts.recordHit('hits').exec();
     res.status(202).type('json').send();
 });
 
+app.get('/events', function(req, res) {
+    rts.getHits('hits', '1hour', 1, function(err:object, data:object) {
+        res.json(data);
+    });
+});
 app.listen(3000);
